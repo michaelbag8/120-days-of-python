ce36974d1c29733dd10013edbe38415232b07d6b
Michael Bag <michaelbag8@gmail.com>
2026-08-02 11:34:52 +0100

    Add basic score grading functionality

---
+++ scores_grader.py
@@ -0,0 +1,15 @@
+#mini grader, will be refactor later
+score = int(input("Enter a score: "))
+
+if score < 0 or score > 100:
+    print("Invalid score")
+elif score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")
