06f92a0d08e951c046988d6a3f1288f91952214d
Michael Bag <michaelbag8@gmail.com>
2026-08-27 19:14:30 +0100

    Correct isinstance typo and improve count_down function
    Fix typo in isinstance and adjust indentation.

--- count_down.py
+++ count_down.py
@@ -7,6 +7,8 @@ while number >= 1:
 
 def count_down():
     number= int(input("Enter a starting number: ")           
+    if not isintance(number, int):
+                return f"{number} is not an integer"          
     if number == 0:
         return 
         print(number)
