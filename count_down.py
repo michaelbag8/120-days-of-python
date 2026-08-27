883b177f75fa7ea4167387c5f62979a4ac9ad005
Michael Bag <michaelbag8@gmail.com>
2026-08-27 19:10:32 +0100

    Refactor count_down function for user input
    Refactor count_down function to take input directly.

--- count_down.py
+++ count_down.py
@@ -5,10 +5,13 @@ while number >= 1:
     print(number)
     number -= 1
 
-def count_down(number):
+def count_down():
+    number= int(input("Enter a starting number: ")           
     if number == 0:
         return 
         print(number)
         count_down(number-1)
+
+                
 print(count_down(5))
 print("Blast Off!")
