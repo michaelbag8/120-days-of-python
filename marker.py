7525299b74d5dae78ee63fcf0bb0321e2fb5807c
Michael Bag <michaelbag8@gmail.com>
2026-08-28 20:47:03 +0100

    Implement word capitalization based on marker
    Added functionality to capitalize previous n words based on a marker in the string.

--- marker.py
+++ marker.py
@@ -25,3 +25,48 @@ for i in range(len(parts)):
 word = " ".join(parts)
 
 print(word)
+
+word = "welcome back home Mr smith (cap, 3) your house is cleaned"
+
+words = word.split()
+
+for i in range(len(words)):
+    if words[i].startswith("(cap,"):
+        marker = words[i]
+
+        # Get the number
+        n = int(marker[5:-1])
+
+        # Capitalize the previous n words
+        for j in range(i - n, i):
+            words[j] = words[j].capitalize()
+
+        # Remove the marker
+        words.pop(i)
+        break
+
+word = " ".join(words)
+
+print(word)
+
+word = "welcome back home Mr smith (cap, 3) your house is cleaned"
+
+match = re.search(r"\(cap,\s*(\d+)\)", word)
+
+if match:
+    n = int(match.group(1))
+
+    words = word.split()
+
+    marker_index = words.index(match.group(0))
+
+    start = marker_index - n
+
+    for i in range(start, marker_index):
+        words[i] = words[i].capitalize()
+
+    words.pop(marker_index)
+
+    word = " ".join(words)
+
+print(word)
