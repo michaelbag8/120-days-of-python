0027ee013a1676c441414d8ddc489fd5ae6a3c62
Michael Bag <michaelbag8@gmail.com>
2026-08-28 20:42:47 +0100

    Add capitalization logic for specific word in marker.py

--- marker.py
+++ marker.py
@@ -11,3 +11,17 @@ before, after = text.split("#", 1)
 result = before.upper() + after
 
 print(result)
+
+word = "welcome back home Mr smith (cap) your house is cleaned"
+
+parts = word.split()
+
+for i in range(len(parts)):
+    if parts[i] == "(cap)":
+        parts[i - 1] = parts[i - 1].capitalize()
+        parts.pop(i)
+        break
+
+word = " ".join(parts)
+
+print(word)
