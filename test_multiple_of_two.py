b56b311d794d8d321c19e78a9832cad0a51faf14
michaelbag8 <michaelbag8@gmail.com>
2026-09-03 22:38:31 +0100

    Added  pytest autouse fixture

--- test_multiple_of_two.py
+++ test_multiple_of_two.py
@@ -64,3 +64,19 @@ def prepare_list(list_length):
 def test_9(prepare_list):
     assert 9 in prepare_list
     assert 10 not in prepare_list
+
+
+#autouse
+@pytest.fixture
+def init_list():
+    return []
+
+# Declare the fixture with autouse
+@pytest.fixture(autouse=True)
+def add_numbers_to_list(init_list):
+    init_list.extend([i for i in range(10)])
+
+# Complete the tests
+def test_elementz(init_list):
+    assert 1 in init_list
+    assert 9 in init_list
