1e2226771eb228d208bef6508ff60d37342a53a9
michaelbag8 <michaelbag8@gmail.com>
2026-09-03 22:07:13 +0100

    Add conditional skip marker

--- test_multiple_of_two.py
+++ test_multiple_of_two.py
@@ -1,4 +1,5 @@
 import pytest
+from datetime import datetime
 
 def multiple_of_two(num):
     if num == 0:
@@ -12,8 +13,29 @@ def test_numbers():
 def test_number():
     assert multiple_of_two(3) == False
 
+# To skip a test using a decorator
+@pytest.mark.skip
 def test_zero():    
   	# Add a context for an exception test here
     with pytest.raises(ValueError):
       	# Check zero input below
         multiple_of_two(0)
+
+@pytest.mark.xfails
+def test_fails():
+    # Write any assert test that will fail
+    assert multiple_of_two(7) == False
+
+
+day_of_week = datetime.now().isoweekday()
+
+def get_unique_values(lst):
+    return list(set(lst))
+
+condition_string = 'day_of_week == 6'
+# Add the conditional skip marker and the string here
+@pytest.mark.skipif(condition_string, reason="Skipped on Saturdays")
+def test_function():
+	# Complete the assertion tests here
+    assert get_unique_values([1,2,3]) == [1,2,3]
+    assert get_unique_values([1,2,3,1]) == [1,2,3]
