683bb1dcc56deb207c39fe2a034b699804765b13
michaelbag8 <michaelbag8@gmail.com>
2026-09-03 22:26:56 +0100

    Added  pytest chain fixture

--- test_multiple_of_two.py
+++ test_multiple_of_two.py
@@ -50,3 +50,17 @@ def prepare_data():
 def test_elements(prepare_data):
     assert 9 in prepare_data
     assert 10 not in prepare_data
+
+#chain fixture
+@pytest.fixture
+def list_length():
+    return 10
+
+# Define the fixture for a list preparation
+@pytest.fixture
+def prepare_list(list_length):
+    return [i for i in range(list_length)]
+
+def test_9(prepare_list):
+    assert 9 in prepare_list
+    assert 10 not in prepare_list
