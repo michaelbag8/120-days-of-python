548b1ed428b23084b6a9f31adca9d13caf9d9093
michaelbag8@ <michaelbag8@gmail.com>
2026-09-04 18:00:06 +0100

    unit test added

--- test_multiple_of_two.py
+++ test_multiple_of_two.py
@@ -80,3 +80,23 @@ def add_numbers_to_list(init_list):
 def test_elementz(init_list):
     assert 1 in init_list
     assert 9 in init_list
+
+
+
+def factorial(n):
+    if n == 0: return 1
+    elif (type(n) == int):
+        return n * factorial(n-1)
+    else: return -1
+
+# Test case: expected input
+def test_regular():
+    assert factorial(5) == 120
+
+# Test case: zero input
+def test_zero():
+    assert factorial(0) == 1
+
+# Test case: input of a wrong type
+def test_str():
+    assert factorial('5') == -1
