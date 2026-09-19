09928ffb75b14976f57b216e1a6426f6be74741c
michaelbag8 <michaelbag8@gmail.com>
2026-09-19 23:55:08 +0100

    add:exercises two - five

--- haven/main.py
+++ haven/main.py
@@ -1,19 +1,71 @@
 import uvicorn
 
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Request, Header
 from fastapi.responses import PlainTextResponse
+from starlette.exceptions import HTTPException
 
 app = FastAPI()
 
+# Exercise 1
 @app.get("/ping", response_class=PlainTextResponse)
 def root():
     return "pong"
 
-@app.get("/hello")
+# Exercise 2
+@app.get("/hello", response_class=PlainTextResponse)
 def hello(name: str = "Guest"):
-    return PlainTextResponse(f"Hello, {name}!")
+    return f"Hello, {name}!"
 
-@app.post("/count")
+# Exercise 3
+@app.post("/count", response_class=PlainTextResponse)
+async def count(request: Request):
+    body = await request.body()
+    return str(len(body))
+
+# For the Get response i need to create a FastAPI exception handler
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    if exc.status_code == 405:
+        return PlainTextResponse (
+            "Send a POST request",
+            status_code=405
+        )
+    return PlainTextResponse(
+        str(exc.detail),
+        status_code=exc.status_code
+    )
+# Exercise 4
+@app.get("/calculate")
+def calculate(a: str, b: str, op: str) -> str:
+    try:
+        a = int(a)
+        b = int(b)
+    except ValueError:
+        raise HTTPException(
+            status_code=400,
+            detail="Bad Request"
+        )
+    if op == "add":
+        return f"Result: {a + b}"
+    
+    elif op == "subtract":
+        return f"Result: {a - b}"
+
+    elif op == "multiply":
+        return f"Result: {a * b}"
+
+    raise HTTPException(
+        status_code=400,
+        detail="Bad Request"
+    )
+
+# Excercise 5
+@app.get("/agent")
+def agent(user_agent: str | None = Header(default=None)):
+    return f"You are visiting us using: {user_agent}"
+
+
 
 
 if __name__ == "__main__":
